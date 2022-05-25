476eb474647b77fd506dc9e16165f2e09514dbbd
tkzw_yuki <gulgul.groove@gmail.com>
2022-05-26 04:03:13 +0900

    初期起動時にDBが起動していない場合があるのでリトライ処理を追加

--- src/database/connect.go
+++ src/database/connect.go
@@ -3,6 +3,7 @@ package database
 import (
 	"log"
 	"os"
+	"time"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -24,7 +25,10 @@ func Connect() {
 	dsn := user + ":" + password + "@tcp(" + host + ":" + port + ")/" + database_name + "?charset=utf8mb4&parseTime=true"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal("FAILED TO CONNECT TO DB")
+		log.Println("FAILED TO CONNECT TO DB")
+		// 初回起動時にDBが起動するまで再起的にDB接続をリトライする
+		time.Sleep(3 * time.Second)
+		Connect()
 	} else {
 		log.Println("CONNECTED TO DB")
 	}
